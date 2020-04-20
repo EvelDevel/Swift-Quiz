fe67bd41351332c0eaa772ecae7855713efea988
Evel-Devel <zhenka_nikitin@mail.ru>
2020-04-20 19:04:49 +0700

    Всякие мелочи

--- Millionaire2.0/Controllers/RecordsViewController.swift
+++ Millionaire2.0/Controllers/RecordsViewController.swift
@@ -42,7 +42,7 @@ extension RecordsViewController: UITableViewDataSource {
         
         let record = Game.shared.records[indexPath.row]
         let dateFormatter = DateFormatter()
-        dateFormatter.dateStyle = .short
+        dateFormatter.dateStyle = .full
         
         if record.percentOfCorrectAnswer! < 30 {
             cell.colorBackground.backgroundColor = #colorLiteral(red: 0.9372549057, green: 0.3490196168, blue: 0.1921568662, alpha: 1)
