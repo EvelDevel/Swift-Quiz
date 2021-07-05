a34f45df3e490df0094a7814b536f77b2da4900d
Evgeniy Nikitin <e.nikitin@dodopizza.com>
2021-07-05 14:53:27 +0700

    General refactoring

--- Swifty-Quiz/AppDelegate.swift
+++ Swifty-Quiz/AppDelegate.swift
@@ -4,11 +4,6 @@
 
 import UIKit
 
-// MARK: Доработки
-
-/// 1. Хранение данных на RealmSwift!
-/// 2. Социальные функции (поделиться историей игр)
-
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
