30734e1d156c9f28bfbf2487f96997b3e00f5f69
Evel-Devel <zhenka_nikitin@mail.ru>
2021-11-14 18:51:41 +0700

    Add progress service

--- Swifty-Quiz/Services/ProgressService.swift
+++ Swifty-Quiz/Services/ProgressService.swift
@@ -6,4 +6,18 @@
 //  Copyright © 2021 Evel-Devel. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+class ProgressService {
+    
+    private let records = Game.shared.getRecordsList()
+    
+    func getProgressBackground(for topic: String) -> UIColor {
+        
+        let grades = [0.05, 0.1, 0.15, 0.2, 0.25, 0.3, 0.35, 0.4, 0.45, 0.5]
+        
+        let color = UIColor(named: "MainYellow") ?? .yellow
+        
+        return color.withAlphaComponent(grades.shuffled().first ?? 0.0)
+    }
+}
