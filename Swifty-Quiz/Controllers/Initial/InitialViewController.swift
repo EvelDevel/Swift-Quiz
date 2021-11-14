1c015b3ef7ccf8f0da0e73936646d346b3d8e79d
Evel-Devel <zhenka_nikitin@mail.ru>
2021-11-14 14:34:33 +0700

    Refactor initial VC

--- Swifty-Quiz/Controllers/Initial/InitialViewController.swift
+++ Swifty-Quiz/Controllers/Initial/InitialViewController.swift
@@ -7,41 +7,41 @@ import StoreKit
 
 class InitialViewController: UIViewController {
 
-	@IBOutlet weak var lastGameTitle: UILabel!
-	@IBOutlet weak var contentCenter: NSLayoutConstraint!
+	@IBOutlet private weak var lastGameTitle: UILabel!
+	@IBOutlet private weak var contentCenter: NSLayoutConstraint!
+    @IBOutlet private weak var donationButton: UIButton!
+	@IBOutlet private weak var totalQuestionsLabel: UILabel!
+	@IBOutlet private weak var selectedTopic: UILabel!
+	@IBOutlet private weak var lastTopic: UILabel!
+	@IBOutlet private weak var lastScore: UILabel!
+	@IBOutlet private weak var totalQuestions: UILabel!
+	@IBOutlet private weak var continueGameButton: UIButton!
+	@IBOutlet private weak var topicPicker: UIButton!
+	@IBOutlet private weak var logoButton: UIButton!
     
-    @IBOutlet weak var donationButton: UIButton!
-    
-	@IBOutlet weak var totalQuestionsLabel: UILabel!
-	@IBOutlet weak var selectedTopic: UILabel!
-	@IBOutlet weak var lastTopic: UILabel!
-	@IBOutlet weak var lastScore: UILabel!
-	@IBOutlet weak var totalQuestions: UILabel!
-	@IBOutlet weak var continueGameButton: UIButton!
-	@IBOutlet weak var topicPicker: UIButton!
-	@IBOutlet weak var logoButton: UIButton!
+	@IBOutlet private var initialWhiteViews: [UIView]!
+	@IBOutlet private var initialButtons: [UIButton]!
 
-	@IBOutlet var initialWhiteViews: [UIView]!
-	@IBOutlet var initialButtons: [UIButton]!
-
-	@IBAction func goToAbout(_ sender: Any) {
+	@IBAction private func goToAbout(_ sender: Any) {
 		SoundPlayer.shared.playSound(sound: .menuMainButton)
 	}
-	@IBAction func tapButtonSounds(_ sender: Any) {
+	@IBAction private func tapButtonSounds(_ sender: Any) {
 		SoundPlayer.shared.playSound(sound: .menuMainButton)
 	}
-    @IBAction func goToDonations(_ sender: Any) {
+    @IBAction private func goToDonations(_ sender: Any) {
         SoundPlayer.shared.playSound(sound: .menuMainButton)
     }
     
-	private let currentAppVersion = Bundle.main.object(forInfoDictionaryKey: "CFBundleShortVersionString") as? String ?? ""
+	private let currentAppVersion = Bundle.main.object(
+        forInfoDictionaryKey: "CFBundleShortVersionString"
+    ) as? String ?? ""
+    
 	private let shadows = Shadow()
 	private let recordCaretaker = RecordsCaretaker()
 
 	override func viewDidLoad() {
 		super.viewDidLoad()
-		setUpInitialInformation()
-		setUpUserInterface()
+		setup()
 	}
 	
 	override func viewWillLayoutSubviews() {
@@ -55,73 +55,76 @@ class InitialViewController: UIViewController {
 }
 
 
-// MARK: Start Information
+// MARK: Main
 extension InitialViewController {
 
-	func setUpInitialInformation() {
-		setStartQuestionSet()
+    private func setup() {
+        setupInformation()
+        setupInterface()
+    }
+    
+    private func setupInformation() {
+		setupStartQuestionSet()
 		updateLastGameInfo()
-		showTotalQuestions()
-	}
-
-	/// Загружаем дефолтный сет
-	func setStartQuestionSet() {
-		if SelectedTopic.shared.topic.questionSet.isEmpty || Game.shared.settings.appLastVersion != currentAppVersion {
-			let newSet = TopicOperator.getTheBasics()
-			SelectedTopic.shared.saveQuestionSet(newSet, topic: "Основы", tag: 11)
-			selectedTopic.text = "Основы"
-			Game.shared.changeContinueStatus()
-		} else {
-			selectedTopic.text = "\(SelectedTopic.shared.topic.topicName)"
-		}
-	}
-
-	/// Устанавливаем информацию о последней игре
-	func updateLastGameInfo() {
-		let records: [Record] = recordCaretaker.getRecordsList()
-		if records.count != 0 {
-			let category = records.first?.topic ?? ""
-			let played = records.first?.playedNum ?? 0
-			let total = records.first?.totalQuestion ?? 0
-			let help = records.first?.helpCounter ?? 0
-			let correct = records.first?.score ?? 0
-			let roundedPercents = String(format: "%.1f", records.first?.percentOfCorrectAnswer ?? 0)
-			lastTopic.text = "Категория: \(category)"
-			totalQuestions.text = "Вопросы: \(played) из \(total) (подсказок: \(help))"
-			lastScore.text = "Правильных ответов: \(correct) (\(roundedPercents)%)"
-		} else {
-			lastTopic.text = "Категория: "
-			totalQuestions.text = "Вопросы: "
-			lastScore.text = "Правильных ответов: "
-		}
-	}
-
-	/// Показываем общее количество вопросов в игре
-	func showTotalQuestions() {
-		totalQuestionsLabel.text = "Вопросов в игре: \(RandomSetManager.showAllQuestionsNumber())"
+		updateTotalQuestionCounter()
 	}
     
-    private func showAnimation() {
-        let pulse = PulseAnimation(numberOfPulse: 10, radius: 23, postion: donationButton.center)
-        pulse.animationDuration = 1.5
-        pulse.backgroundColor = #colorLiteral(red: 0.9254902005, green: 0.2352941185, blue: 0.1019607857, alpha: 1)
-        view.layer.insertSublayer(pulse, below: donationButton.layer)
+    private func setupInterface() {
+        updateContinueButton()
+        addShadows()
+        imageTuning(button: topicPicker, position: .center)
+        imageTuning(button: logoButton, position: .top)
     }
 }
 
 
-// MARK: User Interface
+// MARK: Information
+extension InitialViewController {
+    
+    /// Загружаем дефолтный сет
+    private func setupStartQuestionSet() {
+        if SelectedTopic.shared.topic.questionSet.isEmpty || Game.shared.settings.appLastVersion != currentAppVersion {
+            let newSet = TopicOperator.getTheBasics()
+            SelectedTopic.shared.saveQuestionSet(newSet, topic: "Основы", tag: 11)
+            selectedTopic.text = "Основы"
+            Game.shared.changeContinueStatus()
+        } else {
+            selectedTopic.text = "\(SelectedTopic.shared.topic.topicName)"
+        }
+    }
+
+    /// Устанавливаем информацию о последней игре
+    private func updateLastGameInfo() {
+        let records: [Record] = recordCaretaker.getRecordsList()
+        if records.count != 0 {
+            let category = records.first?.topic ?? ""
+            let played = records.first?.playedNum ?? 0
+            let total = records.first?.totalQuestion ?? 0
+            let help = records.first?.helpCounter ?? 0
+            let correct = records.first?.score ?? 0
+            let roundedPercents = String(format: "%.1f", records.first?.percentOfCorrectAnswer ?? 0)
+            lastTopic.text = "Категория: \(category)"
+            totalQuestions.text = "Вопросы: \(played) из \(total) (подсказок: \(help))"
+            lastScore.text = "Правильных ответов: \(correct) (\(roundedPercents)%)"
+        } else {
+            lastTopic.text = "Категория: "
+            totalQuestions.text = "Вопросы: "
+            lastScore.text = "Правильных ответов: "
+        }
+    }
+
+    /// Показываем общее количество вопросов в игре
+    private func updateTotalQuestionCounter() {
+        totalQuestionsLabel.text = "Вопросов в игре: \(RandomSetManager.showAllQuestionsNumber())"
+    }
+}
+
+
+// MARK: Interface
 extension InitialViewController {
 
-	func setUpUserInterface() {
-		updateContinueButton()
-		addShadows()
-		imageTuning(button: topicPicker, position: .center)
-		imageTuning(button: logoButton, position: .top)
-	}
-
 	/// Показываем или скрываем кноп "продолжить"
-	func updateContinueButton() {
+    private func updateContinueButton() {
 		if Game.shared.records.count != 0 && Game.shared.records[0].continueGameStatus == true {
             
 			if self.continueGameButton.isHidden == true {
@@ -140,7 +143,7 @@ extension InitialViewController {
 	}
 	
 	/// Показываем корректный заголовок последней игры
-	func updateLastGameLabel() {
+    private func updateLastGameLabel() {
 		lastGameTitle.text = "Информация о прошлой игре: "
 		if Game.shared.records.count != 0 {
 			if Game.shared.records.first?.continueGameStatus ?? false {
@@ -150,21 +153,29 @@ extension InitialViewController {
 	}
 
 	/// Добавляем тени на элементы
-	func addShadows() {
+    private func addShadows() {
 		shadows.addStaticShadows(initialWhiteViews)
 		shadows.addButtonShadows(initialButtons)
 	}
 
 	/// Корректного отображение дополнительных картинок у кнопок
-	func imageTuning(button: UIButton, position: UIControl.ContentVerticalAlignment) {
+    private func imageTuning(button: UIButton, position: UIControl.ContentVerticalAlignment) {
 		button.imageView!.contentMode = .scaleAspectFit
 		button.contentVerticalAlignment = position
 		button.contentHorizontalAlignment = .right
 	}
+    
+    /// Donation button animation
+    private func showAnimation() {
+        let pulse = PulseAnimation(numberOfPulse: 10, radius: 23, postion: donationButton.center)
+        pulse.animationDuration = 1.5
+        pulse.backgroundColor = #colorLiteral(red: 0.9254902005, green: 0.2352941185, blue: 0.1019607857, alpha: 1)
+        view.layer.insertSublayer(pulse, below: donationButton.layer)
+    }
 }
 
 
-// MARK: Сетаем себя как делегат
+// MARK: Set delegates
 extension InitialViewController {
 
 	override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
@@ -190,7 +201,7 @@ extension InitialViewController {
 }
 
 
-// MARK: Выполнение функций делегата
+// MARK: Handle delegates
 extension InitialViewController: GameViewControllerDelegate,
                                  TopicViewControllerDelegate,
                                  RecordsViewControllerDelegate,
