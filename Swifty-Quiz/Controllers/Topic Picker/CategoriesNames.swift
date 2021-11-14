b068e5fc12029c3ac3e99928abb77de9c11d37b0
Evel-Devel <zhenka_nikitin@mail.ru>
2021-11-14 22:19:38 +0700

    Add categories names enum

---
+++ Swifty-Quiz/Controllers/Topic Picker/CategoriesNames.swift
@@ -0,0 +1,69 @@
+//
+//  CategoriesNames.swift
+//  Swifty-Quiz
+//
+//  Created by Евгений Никитин on 14.11.2021.
+//  Copyright © 2021 Evel-Devel. All rights reserved.
+//
+
+import Foundation
+
+enum CategoriesNames: String {
+    
+    case random20 = "20 случайных вопросов"
+    case random50 = "50 случайных вопросов"
+    case random100 = "100 случайных вопросов"
+    case deathMatch = "The Death Match"
+        
+    case basic = "Основы"
+    case integersAndBooleans = "Числовые и логические типы"
+    case basicOperators = "Базовые операторы"
+    case rangeOperators = "Операторы диапазона"
+    case booleanOperators = "Логические операторы"
+    case stringAndCharacters = "Строки и символы"
+    case collections = "Коллекции, массивы"
+    case sets = "Множества"
+    case dictionaries = "Словари"
+    case tuples = "Кортежи"
+    case controlFlow = "Управление потоком"
+    case optionals = "Опциональные типы"
+    case functions = "Функции"
+    case closures = "Замыкания"
+    case enums = "Перечисления"
+    case structuresAndClasses = "Структуры и классы"
+    case properties = "Свойства"
+    case methods = "Методы"
+    case subscripts = "Сабскрипты"
+    case inheritance = "Наследование"
+    case initialization = "Инициализация"
+    case deinitialization = "Деинициализация"
+    case errorHandling = "Обработка ошибок"
+    case concurrency = "Согласованность"
+    case optionalChaining = "Опциональная последовательность"
+    case arc = "Автоматический подсчет ссылок"
+    case typeCasting = "Приведение типов"
+    case nestedTypes = "Вложенные типы"
+    case extensions = "Расширения"
+    case opaqueType =  "Непрозрачные типы"
+    case generics = "Универсальные шаблоны"
+    case protocols = "Протоколы"
+    case accessControl = "Контроль доступа"
+    case memorySafety = "Безопасность хранения"
+    case advancedOperators = "Продвинутые операторы"
+        
+    case patterns = "Паттерны: Общие вопросы"
+    case creationalPatterns = "Порождающие паттерны"
+    case structuralPatterns = "Структурные паттерны"
+    case behavioralPatterns = "Поведенческие паттерны"
+    case antipatterns = "Антипаттерны"
+        
+    case extremeProgramming = "Extreme Programming"
+    case masvs = "Мобильная безопасность"
+    case multithreading = "Многопоточность"
+        
+    case controllerLifecycle = "View Controller Lifecycle"
+    case appLifecycle = "App Lifecycle"
+    case boundsAndFrame = "Bounds and Frame"
+    case uiview = "UIView"
+    case autoLayout = "Auto Layout"
+}
