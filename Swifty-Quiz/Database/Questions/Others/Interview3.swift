10b4b86be68590806feff7cd729c333d12bb9bec
Evgeny Nikitin <enikitin@icerockdev.com>
2023-08-09 21:09:33 +0700

    Refactor closures, add question

--- Swifty-Quiz/Database/Questions/Others/Interview3.swift
+++ Swifty-Quiz/Database/Questions/Others/Interview3.swift
@@ -12,19 +12,26 @@ final class Interview3 {
     static func getQuestions() -> [Question] {
         return [
             Question(question: [
-                "000_000_000"
+                "Что мы получим в консоль?"
             ],
-                     image: "",
-                     optionA: "",
-                     optionB: "",
-                     optionC: "",
+                     image: "Closures05",
+                     optionA: "5, 10",
+                     optionB: "5, 5",
+                     optionC: "Получим ошибку",
                      optionD: "",
-                     id: 000_000_000,
+                     id: 876333566,
                      links: [
-                        ""
+                        "https://swiftbook.ru/content/languageguide/closures/",
+                        "https://docs.swift.org/swift-book/documentation/the-swift-programming-language/closures/"
                      ],
                      helpText: """
-                     000_000_000
+                     5, 10. Мы создаем параметр типа Int и выводим его значение на консоль с помощью замыкания.
+                     
+                     Int — это структура (value type), значение которого должно передаваться копированием. Но при этом замыкание всегда выводит актуальное значение параметра.
+                     
+                     Это говорит о том, что внутри замыкания используется не копия параметра a, а ссылка на него.
+                     
+                     Это пример того, как значимый тип данных может быть передан по ссылке, замыкания всегда захватывают значения по ссылке, а не копированием, даже value type.
                      """
                     ),
         ]
